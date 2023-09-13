e6b54b5cde1830699d183c35daa64415a93d9799
SahilSRawat <simransahil2113@gmail.com>
2023-09-12 22:09:27 -0700

    Change name and first element
    Change the name variable and add a new element

--- Chapter 1/echo-shorthand.php
+++ Chapter 1/echo-shorthand.php
@@ -1,6 +1,6 @@
 <?php 
-$name      = 'Ivy';
-$favorites = ['Chocolate', 'Toffee', 'Fudge',];
+$name      = 'Sahil';
+$favorites = ['Caramel', 'Chocolate', 'Toffee', 'Fudge',];
 ?>
 <!DOCTYPE html>
 <html>
